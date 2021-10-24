34816128d7256a3fcf0bfed4734136910598ecdd
oguzhan.orh1907@gmail.com <oguzhan.orh1907@gmail.com>
2021-10-24 14:20:51 +0300

    24 Ekim

---
+++ src/app.js
@@ -0,0 +1,11 @@
+let konutKredileri = ["Konut kredisi","Emlak konut kredisi","Kamut konut kredisi"]
+
+console.log("<ul>")
+for (let i = 0; i <konutKredileri.length; i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+
+
+console.log(konutKredileri)
